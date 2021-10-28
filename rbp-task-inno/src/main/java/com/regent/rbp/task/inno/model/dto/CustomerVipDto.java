b1b6f4c951aa872d2708fbf8aa461ec37f12a0aa
ahxuxing@qq.com <ahxuxing@qq.com>
2021-10-28 16:10:19 +0800

    Fix: 解决CustomerVipDto属性第一个字母大写导致json反序列化出问题。

--- rbp-task-inno/src/main/java/com/regent/rbp/task/inno/model/dto/CustomerVipDto.java
+++ rbp-task-inno/src/main/java/com/regent/rbp/task/inno/model/dto/CustomerVipDto.java
@@ -1,5 +1,6 @@
 package com.regent.rbp.task.inno.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -8,94 +9,123 @@ import java.util.List;
 
 @Data
 public class CustomerVipDto {
-
+    @JsonProperty("VIP")
     @ApiModelProperty(notes = "Vip卡号")
     private String VIP;
 
+    @JsonProperty("BegainDate")
     @ApiModelProperty(notes = "发卡日期")
     private String BegainDate;
 
+    @JsonProperty("ExpireDate")
     @ApiModelProperty(notes = "失效日期")
     private String ExpireDate;
 
+    @JsonProperty("Name")
     @ApiModelProperty(notes = "姓名")
     private String Name;
 
+    @JsonProperty("Sex")
     @ApiModelProperty(notes = "性别，男、女")
     private String Sex;
 
+    @JsonProperty("BirthDate")
     @ApiModelProperty(notes = "出生日期，例‘1995-05-13’")
     private String BirthDate;
 
+    @JsonProperty("Tel")
     @ApiModelProperty(notes = "电话")
     private String Tel;
 
+    @JsonProperty("Email")
     @ApiModelProperty(notes = "电子邮件")
     private String Email;
 
+    @JsonProperty("Address")
     @ApiModelProperty(notes = "地址")
     private String Address;
 
+    @JsonProperty("Discount")
     @ApiModelProperty(notes = "折扣")
     public BigDecimal Discount;
 
+    @JsonProperty("Remark")
     @ApiModelProperty(notes = "备注")
     private String Remark;
 
+    @JsonProperty("Work")
     @ApiModelProperty(notes = "职业")
     private String Work;
 
+    @JsonProperty("MobileTel")
     @ApiModelProperty(notes = "手机")
     private String MobileTel;
 
+    @JsonProperty("LeiBie")
     @ApiModelProperty(notes = "类别")
     private String LeiBie;
 
+    @JsonProperty("Customer_ID")
     @ApiModelProperty(notes = "店铺编号")
     private String Customer_ID;
 
+    @JsonProperty("Group")
     @ApiModelProperty(notes = "组")
     private String Group;
 
+    @JsonProperty("VipGrade")
     @ApiModelProperty(notes = "等级，可设置开卡等级")
     private String VipGrade;
 
+    @JsonProperty("UseStatus")
     @ApiModelProperty(notes = "使用状态")
     private String UseStatus;
 
+    @JsonProperty("HisIntegral")
     @ApiModelProperty(notes = "历史积分")
     public BigDecimal HisIntegral;
 
+    @JsonProperty("City")
     @ApiModelProperty(notes = "省")
     private String City;
 
+    @JsonProperty("InCity")
     @ApiModelProperty(notes = "市")
     private String InCity;
 
+    @JsonProperty("OriginalCustomer")
     @ApiModelProperty(notes = "原渠道")
     private String OriginalCustomer;
 
+    @JsonProperty("Status")
     @ApiModelProperty(notes = "状态，正常或者挂失")
     private String Status;
 
+    @JsonProperty("Posted")
     @ApiModelProperty(notes = "审核，审核了线下不能修改资料")
     public Integer Posted;
 
+    @JsonProperty("Passwords")
     @ApiModelProperty(notes = "密码")
     private String Passwords;
 
+    @JsonProperty("Integral")
     @ApiModelProperty(notes = "积分")
     public BigDecimal Integral;
 
+    @JsonProperty("BusinessManID")
     @ApiModelProperty(notes = "营业员")
     private String BusinessManID;
 
+    @JsonProperty("OldVip")
     @ApiModelProperty(notes = "旧卡号")
     private String OldVip;
 
+    @JsonProperty("Age")
     @ApiModelProperty(notes = "年龄")
     private String Age;
 
+    @JsonProperty("CardType")
     @ApiModelProperty(notes = "卡号类型")
     public Integer CardType;
 
