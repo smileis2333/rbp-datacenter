4a7a1c2bf73c8b921bf7a47c08983b8757629c3b
ahxuxing@qq.com <ahxuxing@qq.com>
2021-11-15 11:02:23 +0800

    feat:增加rbp标准功能的定时任务

---
+++ rbp-task-standard/src/test/java/com/regent/rbp/task/standard/RbpTaskStandardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regent.rbp.task.standard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbpTaskStandardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
