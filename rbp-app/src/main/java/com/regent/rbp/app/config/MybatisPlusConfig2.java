a2e34ea9b093bfa910fd89b28eb3c09a276d1b83
huangjie <huangjie@regentsoft.net>
2022-04-13 10:11:52 +0800

    fix: 废弃mybatis的直接配置，改用customizer，防止各类配置无法自动配置

---
+++ rbp-app/src/main/java/com/regent/rbp/app/config/MybatisPlusConfig2.java
@@ -0,0 +1,92 @@
+package com.regent.rbp.app.config;
+
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.autoconfigure.MybatisPlusPropertiesCustomizer;
+import com.baomidou.mybatisplus.autoconfigure.SpringBootVFS;
+import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
+import org.apache.ibatis.io.VFS;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.core.type.classreading.CachingMetadataReaderFactory;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.core.type.classreading.MetadataReaderFactory;
+import org.springframework.util.ClassUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * @author huangjie
+ * @date : 2022/04/01
+ * @description
+ */
+@Configuration
+@MapperScan(basePackages = "com.regent.rbp.**.dao")
+public class MybatisPlusConfig2 {
+    @Bean
+    public ConfigurationCustomizer mbconfig() {
+        return (properties) -> {
+            properties.setCallSettersOnNulls(true);
+        };
+    }
+
+    @Bean
+    public MybatisPlusPropertiesCustomizer mbpconfig() {
+        return (properties) -> {
+            String typeAliasesPackage = properties.getTypeAliasesPackage();
+            properties.setTypeAliasesPackage(setTypeAliasesPackage(typeAliasesPackage));
+            VFS.addImplClass(SpringBootVFS.class);
+        };
+    }
+
+    @Bean
+    public IdentifierGenerator identifierGenerator() {
+        return new MyBatisPlusConfig.CustomerIdGenerator();
+    }
+
+
+    public static String setTypeAliasesPackage(String typeAliasesPackage) {
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        MetadataReaderFactory metadataReaderFactory = new CachingMetadataReaderFactory(resolver);
+        List<String> allResult = new ArrayList<>();
+        try {
+            for (String aliasesPackage : typeAliasesPackage.split(",")) {
+                List<String> result = new ArrayList<>();
+                aliasesPackage = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX
+                        + ClassUtils.convertClassNameToResourcePath(aliasesPackage.trim()) + "/" + "**/*.class";
+                Resource[] resources = resolver.getResources(aliasesPackage);
+                if (resources != null && resources.length > 0) {
+                    MetadataReader metadataReader = null;
+                    for (Resource resource : resources) {
+                        if (resource.isReadable()) {
+                            metadataReader = metadataReaderFactory.getMetadataReader(resource);
+                            try {
+                                result.add(Class.forName(metadataReader.getClassMetadata().getClassName()).getPackage().getName());
+                            } catch (ClassNotFoundException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+                if (result.size() > 0) {
+                    HashSet<String> hashResult = new HashSet<String>(result);
+                    allResult.addAll(hashResult);
+                }
+            }
+            if (allResult.size() > 0) {
+                typeAliasesPackage = String.join(",", (String[]) allResult.toArray(new String[0]));
+            } else {
+                throw new RuntimeException("mybatis typeAliasesPackage 路径扫描错误,参数typeAliasesPackage:" + typeAliasesPackage + "未找到任何包");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return typeAliasesPackage;
+    }
+}
