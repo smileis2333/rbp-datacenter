3ae09aa514e4a2da6bac3d3b757e6090e83c1668
huangjie <huangjie@regentsoft.net>
2022-04-21 10:33:43 +0800

    feat: spring.profiles.active=dev激活懒加载和排除自动配置 + 调整日志归档大小 + 排除打包目录

---
+++ rbp-app/src/main/java/com/regent/rbp/app/config/dev/DevConfig.java
@@ -0,0 +1,74 @@
+package com.regent.rbp.app.config.dev;
+
+import com.gitee.starblues.factory.process.pipe.extract.ExtractFactory;
+import de.codecentric.boot.admin.client.config.SpringBootAdminClientAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.JvmMetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.LogbackMetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.export.atlas.AtlasMetricsExportAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
+import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchAutoConfiguration;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.reactive.WebSocketReactiveAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketMessagingAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration;
+import org.springframework.cloud.bus.BusAutoConfiguration;
+import org.springframework.cloud.netflix.archaius.ArchaiusAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
+import org.springframework.cloud.netflix.ribbon.RibbonAutoConfiguration;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.cloud.sleuth.annotation.SleuthAnnotationAutoConfiguration;
+import org.springframework.cloud.sleuth.zipkin2.ZipkinAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * @author huangjie
+ * @date : 2022/04/01
+ * @description
+ */
+@Profile("dev")
+@Import(cn.hutool.extra.spring.SpringUtil.class)
+@Configuration
+@EnableAutoConfiguration(exclude = {
+        JacksonAutoConfiguration.class,
+        DataSourceAutoConfiguration.class,
+        SecurityAutoConfiguration.class,
+        JvmMetricsAutoConfiguration.class,
+        LogbackMetricsAutoConfiguration.class,
+        MetricsAutoConfiguration.class,
+        RabbitAutoConfiguration.class,
+        ActiveMQAutoConfiguration.class,
+        AtlasMetricsExportAutoConfiguration.class,
+        AtlasMetricsExportAutoConfiguration.class,
+        BusAutoConfiguration.class,
+        CacheAutoConfiguration.class,
+        ElasticsearchAutoConfiguration.class,
+        MongoAutoConfiguration.class,
+        EurekaClientAutoConfiguration.class,
+        FeignAutoConfiguration.class,
+        SleuthAnnotationAutoConfiguration.class,
+        ZipkinAutoConfiguration.class,
+        SecurityAutoConfiguration.class,
+        SpringBootAdminClientAutoConfiguration.class,
+        WebSocketServletAutoConfiguration.class,
+        WebSocketMessagingAutoConfiguration.class,
+        WebSocketReactiveAutoConfiguration.class,
+        ArchaiusAutoConfiguration.class,
+        RibbonAutoConfiguration.class,
+        SleuthAnnotationAutoConfiguration.class
+})
+public class DevConfig {
+    @Bean
+    public ExtractFactory extractFactory(){
+        return ExtractFactory.getInstant();
+    }
+}
---
+++ rbp-app/src/main/java/com/regent/rbp/app/config/dev/LazyProcessor.java
@@ -0,0 +1,41 @@
+package com.regent.rbp.app.config.dev;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.util.Iterator;
+
+import static org.springframework.beans.factory.config.BeanDefinition.ROLE_APPLICATION;
+
+/**
+ * @author huangjie
+ * @date : 2022/04/12
+ * @description
+ */
+@Profile("dev")
+@Component
+public class LazyProcessor implements BeanFactoryPostProcessor {
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        Iterator<String> beanNames = beanFactory.getBeanNamesIterator();
+        beanNames.forEachRemaining(beanName -> {
+            try {
+                BeanDefinition beanDefinition = beanFactory.getBeanDefinition(beanName);
+                int role = beanDefinition.getRole();
+                if (role == ROLE_APPLICATION) {
+                    beanDefinition.setLazyInit(true);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        });
+        beanFactory.getBeanDefinition("pluginBeanConfig").setLazyInit(false);
+        beanFactory.getBeanDefinition("applicationContextUtil").setLazyInit(false);
+    }
+}
